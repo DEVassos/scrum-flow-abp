325cbe49aeb859c8fdaef65939db0c8e69d39cf5
Henrique.Camargo <henriqueptbd@gmail.com>
2026-05-29 21:25:36 -0300

    fix: ajuste na pagina resultado para aparecer questoes certas e erradas da prova realizada

--- app/public/assets/js/questoes.js
+++ app/public/assets/js/questoes.js
@@ -357,6 +357,7 @@ function irParaResultado() {
       total: totalRespondidas,
       percentual,
       modulo: obterNivelInfo(idModulo),
+      idModulo,
       data: new Date().toLocaleDateString("pt-BR"),
       status: percentual >= 60 ? "aprovado" : "reprovado",
     }),
